3d0b8f2bd8d32723de3e2d59ced6f95679052373
ZhuoXiongHuang <zhuoxionghuang@qq.com>
2024-11-13 03:07:43 +0800

    feat: add Expr

---
+++ src/main/java/top/hzx/lox/ast/Expr.java
@@ -0,0 +1,86 @@
+package top.hzx.lox.ast;
+
+import java.util.List;
+
+import lombok.Getter;
+import top.hzx.lox.common.Token;
+
+@SuppressWarnings("unused")
+public abstract class Expr {
+
+    public interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    @Getter
+    public static class Binary extends Expr {
+
+        private final Expr left;
+        private final Token operator;
+        private final Expr right;
+
+        public Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+    }
+
+    @Getter
+    public static class Grouping extends Expr {
+
+        private final Expr expression;
+
+        public Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+    }
+
+    @Getter
+    public static class Literal extends Expr {
+
+        private final Object value;
+
+        public Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+    }
+
+    @Getter
+    public static class Unary extends Expr {
+
+        private final Token operator;
+        private final Expr right;
+
+        public Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+    }
+
+    public abstract <R> R accept(Visitor<R> visitor);
+
+}
